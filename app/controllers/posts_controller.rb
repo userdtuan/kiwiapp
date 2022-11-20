f105fa23ead65cca5ff88e30fc03b38193050404
userdtuan <dttuan.19it3@vku.udn.vn>
2022-11-20 21:57:05 +0700

    model workaround

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -66,6 +66,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :description, :kind, :author)
+      params.require(:post).permit(:title, :description, :kind, :author, :category_id)
     end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,9 @@
 class Category < ApplicationRecord
+    has_many :posts
+
+    enum category_type: {
+      "General" => 0,
+      "News" => 1,
+      "Sports" => 2,
+    }
 end
