82b99c86e0fcc8a577ccea0d7c6b49baea4acc5d
userdtuan <dttuan.19it3@vku.udn.vn>
2022-11-24 11:16:38 +0700

    add book table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_22_164024) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_24_041120) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.float "price", default: 0.0
+    t.datetime "publish_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
