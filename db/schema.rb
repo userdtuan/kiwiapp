d23ada592d506f27d45b8ad65c42be190708263b
userdtuan <dttuan.19it3@vku.udn.vn>
2023-01-06 09:34:31 +0700

    new changes

--- db/schema.rb
+++ db/schema.rb
@@ -12,28 +12,49 @@
 
 ActiveRecord::Schema[7.0].define(version: 2022_11_24_083526) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "btree_gin"
+  enable_extension "btree_gist"
+  enable_extension "citext"
+  enable_extension "cube"
+  enable_extension "dblink"
+  enable_extension "dict_int"
+  enable_extension "dict_xsyn"
+  enable_extension "earthdistance"
+  enable_extension "fuzzystrmatch"
+  enable_extension "hstore"
+  enable_extension "intarray"
+  enable_extension "ltree"
+  enable_extension "pg_stat_statements"
+  enable_extension "pg_trgm"
+  enable_extension "pgcrypto"
+  enable_extension "pgrowlocks"
+  enable_extension "pgstattuple"
   enable_extension "plpgsql"
+  enable_extension "tablefunc"
+  enable_extension "unaccent"
+  enable_extension "uuid-ossp"
+  enable_extension "xml2"
 
   create_table "authors", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name"
     t.text "brief"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "books", force: :cascade do |t|
-    t.string "title", null: false
+    t.string "title"
     t.text "description"
-    t.float "price", default: 0
+    t.float "price", default: 0.0
     t.date "publish_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "author_id", null: false
+    t.integer "author_id"
     t.integer "category_id"
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "title", null: false
+    t.string "title"
     t.text "description"
     t.string "category_type"
     t.datetime "created_at", null: false
@@ -41,36 +62,36 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_24_083526) do
   end
 
   create_table "order_details", force: :cascade do |t|
-    t.integer "book_id", null: false
-    t.integer "order_id", null: false
-    t.integer "quantity",default: 1
+    t.integer "book_id"
+    t.integer "order_id"
+    t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "orders", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.text "address", null: false
+    t.integer "user_id"
+    t.text "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "title", null: false
+    t.string "title"
     t.text "description"
     t.string "kind"
-    t.integer "author_id", null: false
+    t.integer "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "category_id", null: false
+    t.integer "category_id"
     t.boolean "status", default: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name"
     t.string "email"
     t.string "phone"
-    t.float "level", null: false
+    t.float "level"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
